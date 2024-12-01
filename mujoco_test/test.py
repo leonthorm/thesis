a304f5996e41fd9f401a45a4d9b60b7297399625
simba <l.thormeyer@posteo.de>
2024-12-01 23:46:03 +0100

    trajectory expert

---
+++ mujoco_test/test.py
@@ -0,0 +1,23 @@
+import yaml
+import numpy as np
+
+
+trajectory_path = "db_cbs_trajectories/swap1_double_integrator_3d_opt.yaml"
+with open(trajectory_path, 'r') as file:
+    trajectory  = yaml.safe_load(file)['result'][0]
+
+mass = 0.1
+states =  np.array(trajectory['states'])
+actions = np.array(trajectory['actions'])
+print(type(states))
+
+state = states[0]
+state_idx = np.where(states == state)[0][0]
+
+if state_idx >= trajectory['num_states']-1:
+    control = np.zeros(3)
+else:
+    control = actions[state_idx] * mass
+
+
+print(control)
