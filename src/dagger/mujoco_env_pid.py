ebfd1f060fc7ea0cee7969e9eff4bd0d0e3c7aa0
simba <l.thormeyer@posteo.de>
2024-12-30 19:41:15 +0100

    hyperparameter

--- src/dagger/mujoco_env_pid.py
+++ src/dagger/mujoco_env_pid.py
@@ -17,258 +17,266 @@ DEFAULT_CAMERA_CONFIG = {
 dirname = os.path.dirname(__file__)
 trajectories_dir = dirname+"/../../trajectories"
 class PointMassEnv(MujocoEnv):
-    metadata = {
-        "render_modes": [
-            "human",
-            "rgb_array",
-            "depth_array",
-        ],
-    }
-    print(trajectories_dir)
-    def __init__(
-        self,
-        # target_state: NDArray[np.float32],
-        traj_file,
-        dagger,
-        # xml_file: str =  "../src/dynamics/point_mass.xml",
-        xml_file: str = dirname + "/../dynamics/point_mass.xml",
-        frame_skip: int = 1,
-        default_camera_config: Dict[str, Union[float, int]] = {},
-        healthy_reward: float = 10.0,
-        reset_noise_scale: float = 0.0,
-        ctrl_cost_weight: float = 0.1,
-        render_mode: str="rgb_array",
-        ** kwargs,
-    ):
-        self.dagger = dagger
-        observation_space = Box(low=-np.inf, high=np.inf, shape=(9,), dtype=np.float64)
-
-
-        MujocoEnv.__init__(
-            self,
-            xml_file,
-            frame_skip,
-            observation_space=observation_space,
-            render_mode=render_mode,
-            default_camera_config=default_camera_config,
-            **kwargs,
-        )
-
-        self.metadata = {
+        metadata = {
             "render_modes": [
                 "human",
                 "rgb_array",
+                "depth_array",
             ],
-            "render_fps": int(np.round(1.0 / self.dt)),
         }
-
-        self.ts, self.pos_d, self.vel_d, self.acc_d, self.jerk_d, self.snap_d = get_trajectory(traj_file)
-        self.target_state = np.ravel([self.pos_d[0], self.vel_d[0], self.acc_d[0]])
-
-
-        self.model.site_pos[self.model.site("target_state").id] = np.array([0, 0, 0])
-        # self.model.site_pos[self.model.site_bodyid[0]] = self.target_state[0:3]
-        self._ctrl_cost_weight = ctrl_cost_weight
-        # for idx in range(10):
-        #     x, y, z, _,_,_ = self.trajectory(idx/10)
-        #     site_name = f"traj_site_{idx}"
-        #     site_id = self.model.site(site_name).id
-        #     self.model.site_pos[site_id] = np.array([x, y, z])
-        #     print(self.model.site_pos[site_id])
-        self.steps = 0
-        self.max_steps = 700
-        self.trajectory = np.array([])
-        self.observations = np.array([])
-        self.actions = np.array([])
-        self.max_t = 2.0
-        self.first_run = True
-        self.traj_name = ''
-        self.set_traj_name(traj_file)
+        print(trajectories_dir)
+        def __init__(
+            self,
+            # target_state: NDArray[np.float32],
+            traj_file,
+            dagger,
+            # xml_file: str =  "../src/dynamics/point_mass.xml",
+            xml_file: str = dirname + "/../dynamics/point_mass.xml",
+            frame_skip: int = 1,
+            default_camera_config: Dict[str, Union[float, int]] = {},
+            healthy_reward: float = 10.0,
+            reset_noise_scale: float = 0.0,
+            ctrl_cost_weight: float = 0.1,
+            render_mode: str="rgb_array",
+            ** kwargs,
+        ):
+            self.dagger = dagger
+            observation_space = Box(low=-np.inf, high=np.inf, shape=(9,), dtype=np.float64)
 
 
+            MujocoEnv.__init__(
+                self,
+                xml_file,
+                frame_skip,
+                observation_space=observation_space,
+                render_mode=render_mode,
+                default_camera_config=default_camera_config,
+                **kwargs,
+            )
 
-    def set_traj(self, traj_file):
-        print('target trajectory', traj_file)
-        self.ts, self.pos_d, self.vel_d, self.acc_d, self.jerk_d, self.snap_d = get_trajectory(traj_file)
-        self.reset_model()
-        self.set_traj_name(traj_file)
+            self.metadata = {
+                "render_modes": [
+                    "human",
+                    "rgb_array",
+                ],
+                "render_fps": int(np.round(1.0 / self.dt)),
+            }
 
-    def set_traj_name(self, traj_file):
-        match = re.search(r"/([^/]+?)0[^/]*$", traj_file)
-        if match:
-            name = match.group(1)
-            print(name)
-            self.traj_name = name
-
-    def step(self, action):
-        # print(self.data.time / self.dt)
-        position_before = self.data.qpos.copy()
-        self.add_state_to_trajectory(action, position_before)
-        self.do_simulation(action, self.frame_skip)
-        position_after = self.data.qpos.copy()
-        truncation = self.set_target_state()
-        # t = self.data.time
-        # traj_des = self.goal_trajectory(t)
-
-        # self.target_state = traj_des
-        observation, reward, done, info = self._get_info(action, position_after, position_before)
+            self.ts, self.pos_d, self.vel_d, self.acc_d, self.jerk_d, self.snap_d = get_trajectory(traj_file)
+            self.target_state = np.ravel([self.pos_d[0], self.vel_d[0], self.acc_d[0]])
 
 
-
-        if self.render_mode == "human":
-            self.render()
-
-        self.steps += 1
-        return (
-            observation,
-            reward,
-            done,
-            # self.steps > self.max_steps,
-            truncation,
-            info
-        )
-
-    def set_target_state(self):
-        truncation = False
-        if self.data.time < self.max_t:
-            pos, vel, acc, _, _ = self.ideal_state_at_time(self.data.time, self.dt)
-            self.target_state = np.ravel([pos, vel, acc])
-        else:
-            truncation = True
-        return truncation
-
-    def ideal_state_at_time(self, t, dt):
-        idx = int(t // dt)
-
-        return self.pos_d[idx], self.vel_d[idx], self.acc_d[idx] , self.jerk_d[idx], self.snap_d[idx]
-
-    def _get_info(self, action, position_after, position_before):
-        distance_to_target = np.linalg.norm(self.target_state[0:3] - position_after)
-        # print(distance_to_target)
-        observation = self._get_obs()
+            self.model.site_pos[self.model.site("target_state").id] = np.array([0, 0, 0])
+            # self.model.site_pos[self.model.site_bodyid[0]] = self.target_state[0:3]
+            self._ctrl_cost_weight = ctrl_cost_weight
+            # for idx in range(10):
+            #     x, y, z, _,_,_ = self.trajectory(idx/10)
+            #     site_name = f"traj_site_{idx}"
+            #     site_id = self.model.site(site_name).id
+            #     self.model.site_pos[site_id] = np.array([x, y, z])
+            #     print(self.model.site_pos[site_id])
+            self.steps = 0
+            self.max_steps = 0
+            self.trajectory = np.array([])
+            self.observations = np.array([])
+            self.actions = np.array([])
+            self.max_t = 2.0
+            self.first_run = True
+            self.traj_name = ''
+            self.set_traj_name(traj_file)
+            self.current_pos_error = 0
+            self.max_steps
 
 
+        def set_traj(self, traj_file):
+            print('target trajectory', traj_file)
+            self.ts, self.pos_d, self.vel_d, self.acc_d, self.jerk_d, self.snap_d = get_trajectory(traj_file)
+            self.reset_model()
+            self.set_traj_name(traj_file)
+            self.max_steps = len(self.ts)
 
-        reward, reward_info = self._get_rew(position_before, position_after, action)
+        def set_traj_name(self, traj_file):
+            match = re.search(r"/([^/]+?)0[^/]*$", traj_file)
+            if match:
+                name = match.group(1)
+                self.traj_name = name
 
-        done = self._is_done(distance_to_target)
-        info = {
-            "position": self.data.qpos,
-            "desired_position": self.target_state[0:3],
-            "velocity": self.data.qvel,
-            "desired_velocity": self.target_state[3:6],
-            "done": done,
-            ** reward_info,
-        }
-        return observation, reward, done, info
+        def step(self, action):
+            # print(self.data.time / self.dt)
+            position_before = self.data.qpos.copy()
+            self.add_state_to_trajectory(action, position_before)
+            self.do_simulation(action, self.frame_skip)
+            position_after = self.data.qpos.copy()
+            truncation = self.set_target_state()
+            # t = self.data.time
+            # traj_des = self.goal_trajectory(t)
 
-    def add_state_to_trajectory(self, action, position_after):
-        observation = self._get_obs()
-        current_velocity = self.data.qvel.copy()
-        current_acceleration = self.data.qacc.copy()
-        # print('acc: '+ str(current_acceleration))
-        self.observations = np.concatenate((self.observations, observation))
-        self.trajectory = np.concatenate(
-            (self.trajectory, position_after, current_velocity, current_acceleration, self.target_state))
-        self.actions = np.concatenate((self.actions, action))
-
-    def goal_trajectory(self, t, radius=1.0, omega=2.0, z_amplitude=1.0, z_freq=0.5):
-
-        x_d = radius * np.cos(omega * t) - radius
-        y_d = radius * np.sin(omega * t)
-        z_d = z_amplitude * np.sin(z_freq * t) 
-        vx_d = -radius * omega * np.sin(omega * t)
-        vy_d = radius * omega * np.cos(omega * t)
-        vz_d = z_amplitude * z_freq * np.cos(z_freq * t)
-        return np.array([x_d, y_d, 0, vx_d, vy_d, 0])
+            # self.target_state = traj_des
+            observation, reward, done, info = self._get_info(action, position_after, position_before)
 
 
-    def reset_model(self):
-        self.set_state(self.pos_d[0], self.vel_d[0])
-        self.model.site_pos[self.model.site("target_state").id] = np.array([0, 0, 0])
-        self.set_target_state()
-        self._save_trajectory()
 
-        self.steps = 0
-        return self._get_obs()
+            if self.render_mode == "human":
+                self.render()
 
-    def _save_trajectory(self):
+            self.steps += 1
+            return (
+                observation,
+                reward,
+                done,
+                # self.steps > self.max_steps,
+                truncation,
+                info
+            )
 
-        if self.trajectory.size != 0:
-            # print("saving trajectory")
-            self.trajectory = np.reshape(self.trajectory, (-1, 18))
+        def set_target_state(self):
+            truncation = False
+            if self.data.time < self.max_t:
+                pos, vel, acc, _, _ = self.ideal_state_at_time(self.data.time, self.dt)
+                self.target_state = np.ravel([pos, vel, acc])
+            else:
+                truncation = True
+            return truncation
 
-            file_name = trajectories_dir+"/"+self.dagger+"/trajectory_"+self.dagger+"_"+self.traj_name+".csv"
-            np.savetxt(file_name, self.trajectory, delimiter=",")
+        def ideal_state_at_time(self, t, dt):
+            idx = int(t // dt)
 
-            if self.first_run and  self.dagger == "dagger":
+            return self.pos_d[idx], self.vel_d[idx], self.acc_d[idx] , self.jerk_d[idx], self.snap_d[idx]
 
-                self.observations = np.reshape(self.observations, (-1, self.observation_space.shape[0]))
-                self.actions = np.reshape(self.trajectory, (-1, 3))
-                data = {
-                    'obs': self.observations,  # (num_samples, obs_dim)
-                    'act': self.actions  # (num_samples, act_dim)
-                }
-                pickle_filename = trajectories_dir+'/dagger/expert_data.pkl'
-                with open(pickle_filename, 'wb') as f:
-                    pickle.dump(data, f)
-                expert_data_filename = trajectories_dir+ "/"+self.dagger +"/trajectory_expert_"+self.traj_name+".csv"
-                np.savetxt(expert_data_filename, self.trajectory,
-                           delimiter=",")
-                self.observations = np.array([])
-                self.actions = np.array([])
-                self.first_run = False
+        def _get_info(self, action, position_after, position_before):
+            distance_to_target = np.linalg.norm(self.target_state[0:3] - position_after)
+            # print(distance_to_target)
+            observation = self._get_obs()
 
-        self.trajectory = np.array([])
 
-    def _get_obs(self):
 
-        pos_error = self.target_state[0:3]-self.data.qpos
-        vel_error = self.target_state[3:6]-self.data.qvel
-        acc_d = self.target_state[6:9]
-        obs = np.concatenate(
-            [pos_error,
-             vel_error,
-             acc_d]).ravel()
+            reward, reward_info = self._get_rew(position_before, position_after, action)
 
-        return obs
+            done = self._is_done(distance_to_target)
+            info = {
+                "position": self.data.qpos,
+                "desired_position": self.target_state[0:3],
+                "velocity": self.data.qvel,
+                "desired_velocity": self.target_state[3:6],
+                "done": done,
+                ** reward_info,
+            }
+            return observation, reward, done, info
 
-    def _is_done(self, distance_to_target):
-        if self.dagger == "dagger":
+        def add_state_to_trajectory(self, action, position_after):
+            observation = self._get_obs()
+            current_velocity = self.data.qvel.copy()
+            current_acceleration = self.data.qacc.copy()
+            # print('acc: '+ str(current_acceleration))
+            self.observations = np.concatenate((self.observations, observation))
+            self.trajectory = np.concatenate(
+                (self.trajectory, position_after, current_velocity, current_acceleration, self.target_state))
+            self.actions = np.concatenate((self.actions, action))
+
+        def goal_trajectory(self, t, radius=1.0, omega=2.0, z_amplitude=1.0, z_freq=0.5):
+
+            x_d = radius * np.cos(omega * t) - radius
+            y_d = radius * np.sin(omega * t)
+            z_d = z_amplitude * np.sin(z_freq * t)
+            vx_d = -radius * omega * np.sin(omega * t)
+            vy_d = radius * omega * np.cos(omega * t)
+            vz_d = z_amplitude * z_freq * np.cos(z_freq * t)
+            return np.array([x_d, y_d, 0, vx_d, vy_d, 0])
+
+
+        def reset_model(self):
+            self.set_state(self.pos_d[0], self.vel_d[0])
+            self.model.site_pos[self.model.site("target_state").id] = np.array([0, 0, 0])
+            self.set_target_state()
+            self._save_trajectory()
+
+            self.steps = 0
+
+            return self._get_obs()
+
+        def _save_trajectory(self):
+
+            if self.trajectory.size != 0:
+                # print("saving trajectory")
+                self.trajectory = np.reshape(self.trajectory, (-1, 18))
+
+                file_name = trajectories_dir+"/"+self.dagger+"/trajectory_"+self.dagger+"_"+self.traj_name+".csv"
+                np.savetxt(file_name, self.trajectory, delimiter=",")
+
+                if self.first_run and  self.dagger == "dagger":
+
+                    self.observations = np.reshape(self.observations, (-1, self.observation_space.shape[0]))
+                    self.actions = np.reshape(self.trajectory, (-1, 3))
+                    data = {
+                        'obs': self.observations,  # (num_samples, obs_dim)
+                        'act': self.actions  # (num_samples, act_dim)
+                    }
+                    pickle_filename = trajectories_dir+"/dagger/trajectory_expert_"+self.traj_name+".pkl"
+                    with open(pickle_filename, 'wb') as f:
+                        pickle.dump(data, f)
+                    expert_data_filename = trajectories_dir+ "/"+self.dagger +"/trajectory_expert_"+self.traj_name+".csv"
+                    np.savetxt(expert_data_filename, self.trajectory,
+                               delimiter=",")
+                    self.observations = np.array([])
+                    self.actions = np.array([])
+                    self.first_run = False
+
+            self.trajectory = np.array([])
+
+        def _get_obs(self):
+
+            pos_error = self.target_state[0:3]-self.data.qpos
+            self.current_pos_error = pos_error
+            vel_error = self.target_state[3:6]-self.data.qvel
+            acc_d = self.target_state[6:9]
+            obs = np.concatenate(
+                [pos_error,
+                 vel_error,
+                 acc_d]).ravel()
+
+            return obs
+
+        def _is_done(self, distance_to_target):
+            if self.dagger == "dagger":
+                return distance_to_target > 0.5
             return distance_to_target > 0.5
-        return distance_to_target > 0.5
 
-    def control_cost(self, action):
-        control_cost = self._ctrl_cost_weight * np.sum(np.square(action))
-        return control_cost
+        def control_cost(self, action):
+            control_cost = self._ctrl_cost_weight * np.sum(np.square(action))
+            return control_cost
 
-    def _get_rew(self, position_before, position_after, action):
+        def _get_rew(self, position_before, position_after, action):
 
-        de_weight = 1.0
-        ve_weight = 0.1
-        distance_before = np.linalg.norm(self.target_state[0:3] - position_before)
-        distance_after = np.linalg.norm(self.target_state[0:3] - position_after)
+            de_weight = 1.0
+            ve_weight = 0.1
+            distance_before = np.linalg.norm(self.target_state[0:3] - position_before)
+            distance_after = np.linalg.norm(self.target_state[0:3] - position_after)
 
-        distance_reward = distance_before - distance_after
+            distance_reward = distance_before - distance_after
 
-        velocity_current = self.data.qvel.copy()
-        velocity_error = -np.linalg.norm(velocity_current)
+            velocity_current = self.data.qvel.copy()
+            velocity_error = -np.linalg.norm(velocity_current)
 
-        ctrl_cost = self.control_cost(action)
+            ctrl_cost = self.control_cost(action)
 
-        reward = distance_reward*de_weight - ctrl_cost + velocity_error * ve_weight
+            reward = distance_reward*de_weight - ctrl_cost + velocity_error * ve_weight
 
-        if distance_after < 0.01:
-            reward += 5.0
+            if distance_after < 0.01:
+                reward += 5.0
 
-        reward_info = {
-            "distance_reward": distance_reward,
-            "control_cost": -ctrl_cost,
-            "velocity_error": velocity_error,
-        }
+            reward_info = {
+                "distance_reward": distance_reward,
+                "control_cost": -ctrl_cost,
+                "velocity_error": velocity_error,
+            }
 
-        return reward, reward_info
+            return reward, reward_info
 
-    def get_dt(self):
-        return self.dt
+        def get_dt(self):
+            return self.dt
 
+        def check_success(self):
+
+            max_steps_reached = self.max_steps-1 >= self.steps
+            close_to_target = self.current_pos_error < 0.3
+            return max_steps_reached and close_to_target
--- src/dagger/run_dagger.py
+++ src/dagger/run_dagger.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import tempfile
 import shutil
@@ -14,6 +15,7 @@ from imitation.algorithms import bc
 from imitation.algorithms.dagger import SimpleDAggerTrainer, DAggerTrainer
 from imitation.util.util import make_vec_env
 from imitation.data import rollout, serialize, types
+from dagger import dagger
 
 dirname = os.path.dirname(__file__)
 training_dir = dirname + "/../../training/dagger"
@@ -28,7 +30,7 @@ wave_traj_file = traj_dir + "wave0.csv"
 rng = np.random.default_rng(0)
 device = torch.device('cpu')
 
-#logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.INFO)
 
 #target_state = np.concatenate([np.random.uniform(0, 0.5, 3), [0.0, 0.0, 0.0]]).flatten()
 # target_state = np.array([0.5,0.25,0.5, 0, 0, 0])
@@ -59,7 +61,7 @@ if __name__ == '__main__':
     pm_venv = make_vec_env(
         env_id,
         rng=rng,
-        n_envs=1,
+        n_envs=2,
         parallel=False
     )
 
@@ -89,69 +91,23 @@ if __name__ == '__main__':
         rng=rng,
         device=device,
     )
-
-
-    dagger_trainer = DAggerTrainer(
-        venv=pm_venv,
-        scratch_dir=training_dir,
-        bc_trainer=bc_trainer,
-        rng=rng,
-    )
+    total_timesteps = 4_000
     rollout_round_min_episodes = 3
     rollout_round_min_timesteps = 200
 
-    total_timesteps = 4_000
-    total_timestep_count = 0
-    round_num = 0
-
-    while total_timestep_count < total_timesteps:
-        print(f"Starting round {total_timestep_count}")
-        round_episode_count = 0
-        round_timestep_count = 0
-
-        collector = dagger_trainer.create_trajectory_collector()
-
-        sample_until = rollout.make_sample_until(
-            min_timesteps=max(rollout_round_min_timesteps, dagger_trainer.batch_size),
-            min_episodes=rollout_round_min_episodes,
-        )
-
-        trajectories = rollout.generate_trajectories(
-            policy=expert,
-            venv=collector,
-            sample_until=sample_until,
-            deterministic_policy=True,
-            rng=collector.rng,
-        )
-
-        for traj in trajectories:
-            dagger_trainer._logger.record_mean(
-                "dagger/mean_episode_reward",
-                np.sum(traj.rews),
-            )
-            round_timestep_count += len(traj)
-            total_timestep_count += len(traj)
-
-        round_episode_count += len(trajectories)
-
-        dagger_trainer._logger.record("dagger/total_timesteps", total_timestep_count)
-        dagger_trainer._logger.record("dagger/round_num", round_num)
-        dagger_trainer._logger.record("dagger/round_episode_count", round_episode_count)
-        dagger_trainer._logger.record("dagger/round_timestep_count", round_timestep_count)
-        print(round_timestep_count)
-        dagger_trainer.extend_and_update()
-        expert.reset_controller()
-        round_num += 1
+    dagger_trainer = dagger(venv=pm_venv,
+                            iters=10,
+                            scratch_dir=training_dir,
+                            device=device,
+                            observation_space=pm_venv.observation_space,
+                            action_space=pm_venv.action_space,
+                            rng=rng, expert=PIDPolicy, total_timesteps=total_timesteps, rollout_round_min_episodes=rollout_round_min_episodes,
+                            rollout_round_min_timesteps=rollout_round_min_timesteps)
 
     reward, _ = evaluate_policy(dagger_trainer.policy, pm_venv, 10)
 
     print(dagger_trainer.save_trainer())
     # print(bc_trainer.save)
 
-
     shutil.rmtree(training_dir + "/demos")
     print("Reward:", reward)
-
-
-
-
