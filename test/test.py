cc1443074263d6039119037cc49f0baf1c546c12
simba <l.thormeyer@posteo.de>
2025-01-10 23:39:26 +0100

    multirobot dynamics

--- test/test.py
+++ test/test.py
@@ -1,7 +1,69 @@
-import sys
-import mujoco_test.test.new
+import mujoco
+import mediapy as media
 
-mujoco_test.test.new.module_func()
 
-for path in sys.path:
-    print(path)
+MJCF = """
+<mujoco>
+  <asset>
+    <texture name="grid" type="2d" builtin="checker" rgb1=".1 .2 .3"
+     rgb2=".2 .3 .4" width="300" height="300" mark="none"/>
+    <material name="grid" texture="grid" texrepeat="1 1"
+     texuniform="true" reflectance=".2"/>
+  </asset>
+
+  <worldbody>
+    <light name="light" pos="0 0 1"/>
+    <geom name="floor" type="plane" pos="0 0 -.5" size="2 2 .1" material="grid"/>
+    <site name="anchor" pos="0 0 .3" size=".01"/>
+    <camera name="fixed" pos="0 -1.3 .5" xyaxes="1 0 0 0 1 2"/>
+
+    <geom name="pole" type="cylinder" fromto=".3 0 -.5 .3 0 -.1" size=".04"/>
+    <body name="bat" pos=".3 0 -.1">
+      <joint name="swing" type="hinge" damping="1" axis="0 0 1"/>
+      <geom name="bat" type="capsule" fromto="0 0 .04 0 -.3 .04"
+       size=".04" rgba="0 0 1 1"/>
+    </body>
+
+    <body name="box_and_sphere" pos="0 0 0">
+      <joint name="free" type="free"/>
+      <geom name="red_box" type="box" size=".1 .1 .1" rgba="1 0 0 1"/>
+      <geom name="green_sphere"  size=".06" pos=".1 .1 .1" rgba="0 1 0 1"/>
+      <site name="hook" pos="-.1 -.1 -.1" size=".01"/>
+      <site name="IMU"/>
+    </body>
+
+    <geom name="pole2" type="cylinder" fromto="-.3 0 -.5 -.3 0 -.1" size=".04"/>
+    <body name="bat2" pos="-.3 0 -.1">
+      <joint name="swing2" type="hinge" damping="1" axis="0 0 1"/>
+      <geom name="bat2" type="capsule" fromto="0 0 .04 0 -.3 .04"
+       size=".04" rgba="0 0 1 1"/>
+    </body>
+  </worldbody>
+
+  <tendon>
+    <spatial name="wire" limited="true" range="0 0.35" width="0.003">
+      <site site="anchor"/>
+      <site site="hook"/>
+    </spatial>
+  </tendon>
+
+  <actuator>
+    <motor name="my_motor" joint="swing" gear="1"/>
+    <motor name="my_motor2" joint="swing2" gear="1"/>
+  </actuator>
+
+  <sensor>
+    <accelerometer name="accelerometer" site="IMU"/>
+  </sensor>
+</mujoco>
+"""
+model = mujoco.MjModel.from_xml_string(MJCF)
+data = mujoco.MjData(model)
+height = 480
+width = 480
+
+with mujoco.Renderer(model, height, width) as renderer:
+  mujoco.mj_forward(model, data)
+  renderer.update_scene(data, "fixed")
+
+  media.show_image(renderer.render())
