5262631dfca315e2083445eaebb03b69e23d80dc
simba <l.thormeyer@posteo.de>
2025-03-11 15:13:08 +0100

    dynobench step function

---
+++ scripts/run_dagger_coltrans_dyno.py
@@ -0,0 +1,200 @@
+import argparse
+import os
+import shutil
+import sys
+
+import dynobench
+import gymnasium as gym
+import numpy as np
+import torch
+from gymnasium.spaces import Box
+from imitation.util.util import make_vec_env
+
+from src.dagger.dagger import dagger_multi_robot, dagger
+from src.thrifty.thrifty import thrifty_multi_robot, thrifty
+# from mujoco_test.generate_swarm import generate_xml_from_start
+from src.util.load_traj import load_model
+
+
+dirname = os.path.dirname(__file__)
+training_dir_dagger = dirname + "/../training/coltrans/dagger"
+training_dir_thrifty = dirname + "/../training/coltrans/thrifty"
+expert_traj_dir = dirname + "/../trajectories/expert_trajectories/coltrans_planning"
+
+
+rng = np.random.default_rng(0)
+device = torch.device('cpu')
+
+# logging.getLogger().setLevel(logging.INFO)
+
+# target_state = np.concatenate([np.random.uniform(0, 0.5, 3), [0.0, 0.0, 0.0]]).flatten()
+# target_state = np.array([0.5,0.25,0.5, 0, 0, 0])
+
+
+beta = 0.2
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--inp",
+        default=None,
+        type=str,
+        help="yaml input reference trajectory",
+        required=True,
+    )
+    # todo: output
+    parser.add_argument(
+        "--out",
+        default=None,
+        type=str,
+        help="yaml output trajectory by policy",
+        required=False,
+    )
+    parser.add_argument(
+        "--model_path", default=None, type=str, required=True, help="number of robots"
+    )
+    # parser.add_argument(
+    #     "-cff", "--enable_cffirmware", action="store_true"
+    # )  # on/off flag    args = parser.args
+    parser.add_argument(
+        "-w", "--write", action="store_true"
+    )  # on/off flag    args = parser.args
+    parser.add_argument(
+        "-a", "--compAcc", action="store_true"
+    )  # on/off flag    args = parser.args
+    parser.add_argument(
+        "-noC", "--nocableTracking", action="store_true"
+    )  # on/off flag    args = parser.args
+
+    parser.add_argument(
+        "-alg", "--daggerAlgorithm", default="dagger", type=str, required=False, help="dagger or thrifty"
+    )
+    parser.add_argument(
+        "-dc", "--decentralizedPolicy", action="store_true"
+    )
+
+    args = parser.parse_args()
+
+    model, num_robots = load_model(args.model_path)
+
+    algorithm = args.daggerAlgorithm
+    decentralized = args.decentralizedPolicy
+
+    # robot = "robot"
+    gym.envs.registration.register(
+        id='dyno_coltrans-v0',
+        entry_point='src.mujoco_envs.dyno_env_coltrans:DynoColtransEnv',
+        kwargs={
+            'model': model,
+            'model_path': args.model_path,
+            'reference_traj_path': args.inp,
+            'num_robots': num_robots,
+        },
+    )
+    demo_dir = os.path.abspath(training_dir_dagger + "/demos")
+
+    if os.path.exists(demo_dir):
+        shutil.rmtree(demo_dir)
+
+    env_id = "dyno_coltrans-v0"
+    venv = make_vec_env(
+        env_id,
+        rng=rng,
+        n_envs=1,
+        parallel=False
+    )
+    # todo: different envs
+
+    # trajs = [forest_2robots, forest_2robots, forest_2robots,
+    #          forest_2robots]
+    #
+    # for idx, env in enumerate(venv.envs):
+    #     attr = env.get_wrapper_attr('set_traj')
+    #     attr(trajs[idx])
+
+    # todo: check this
+    total_timesteps = 4_000
+    rollout_round_min_episodes = 3
+    rollout_round_min_timesteps = 200
+    iters = 20
+    observation_space_size = (6
+                              + 6 * num_robots
+                              + 7 * num_robots
+                              )
+
+    observation_space = Box(low=-np.inf, high=np.inf,
+                            shape=(observation_space_size,), dtype=np.float64)
+    action_space = Box(low=0, high=0.14, shape=(4 * num_robots,), dtype=np.float64)
+
+    if decentralized:
+        if algorithm == 'dagger':
+            dagger_trainer = dagger_multi_robot(venv=venv,
+                                                iters=iters,
+                                                scratch_dir=training_dir_dagger,
+                                                device=device,
+                                                observation_space=observation_space,
+                                                action_space=action_space,
+                                                rng=rng, expert_policy='ColtransPolicy',
+                                                total_timesteps=total_timesteps,
+                                                rollout_round_min_episodes=rollout_round_min_episodes,
+                                                rollout_round_min_timesteps=rollout_round_min_timesteps,
+                                                num_robots=num_robots, )
+            # todo reward
+            # reward, _ = evaluate_policy(dagger_trainer.policy, pm_venv, 10)
+            print(dagger_trainer.save_trainer())
+
+        if algorithm == 'thrifty':
+            thrifty_trainer = thrifty_multi_robot(venv=venv,
+                                                  iters=iters,
+                                                  scratch_dir=training_dir_thrifty,
+                                                  device=device,
+                                                  observation_space=observation_space,
+                                                  action_space=action_space,
+                                                  rng=rng, expert_policy='ColtransPolicy',
+                                                  total_timesteps=total_timesteps,
+                                                  rollout_round_min_episodes=rollout_round_min_episodes,
+                                                  rollout_round_min_timesteps=rollout_round_min_timesteps,
+                                                  n_robots=num_robots, )
+
+            # reward, _ = evaluate_policy(thrifty_trainer.policy, pm_venv, 10)
+            print(thrifty_trainer.save_trainer())
+    else:
+        if algorithm == 'dagger':
+            dagger_trainer = dagger(venv=venv,
+                                    iters=iters,
+                                    scratch_dir=training_dir_dagger,
+                                    device=device,
+                                    observation_space=observation_space,
+                                    action_space=action_space,
+                                    rng=rng, expert_policy='ColtransPolicy',
+                                    total_timesteps=total_timesteps,
+                                    rollout_round_min_episodes=rollout_round_min_episodes,
+                                    rollout_round_min_timesteps=rollout_round_min_timesteps,
+                                    num_robots=num_robots)
+            # todo reward
+            # reward, _ = evaluate_policy(dagger_trainer.policy, pm_venv, 10)
+            print(dagger_trainer.save_trainer())
+
+        if algorithm == 'thrifty':
+            thrifty_trainer = thrifty(venv=venv,
+                                      iters=iters,
+                                      scratch_dir=training_dir_thrifty,
+                                      device=device,
+                                      observation_space=observation_space,
+                                      action_space=action_space,
+                                      rng=rng, expert_policy='ColtransPolicy',
+                                      total_timesteps=total_timesteps,
+                                      rollout_round_min_episodes=rollout_round_min_episodes,
+                                      rollout_round_min_timesteps=rollout_round_min_timesteps,
+                                      num_robots=num_robots, )
+            print(thrifty_trainer.save_trainer())
+
+
+    # if thrifty_algo and dagger_algo:
+    #     evaluate_policy(thrifty_trainer.policy, pm_venv, 10)
+    #     evaluate_policy(dagger_trainer.policy, pm_venv, 10)
+    # print(bc_trainer.save)
+
+    # shutil.rmtree(training_dir + "/demos")
+    # print("Reward:", reward)
--- src/mujoco_envs/dyno_env_coltrans.py
+++ src/mujoco_envs/dyno_env_coltrans.py
@@ -1,32 +1,152 @@
-import gymnasium as gym
+from typing import Optional
+
 import dynobench
+import gymnasium as gym
+import numpy as np
+from gymnasium.spaces import Box
+
+from src.util.load_traj import load_coltans_traj
+
 
 class DynoColtransEnv(gym.Env):
-    def __init__(self):
+    def __init__(
+            self,
+            model,
+            model_path,
+            reference_traj_path,
+            num_robots
+    ):
         super().__init__()
 
+        self.num_robots = num_robots
+        self.action_space = Box(low=0, high=0.14, shape=(4 * self.num_robots,), dtype=np.float64)
+        observation_space_size = (6
+                                  + 6 * self.num_robots
+                                  + 7 * self.num_robots
+                                  )
+
+        self.observation_space = Box(low=-np.inf, high=np.inf,
+                                shape=(observation_space_size,), dtype=np.float64)
+        self.robot = dynobench.robot_factory(
+            model_path, [-1000, -1000, -1.0], [1000, 1000, 1.0]
+        )
+        self.steps = 0
+        self.states_d, self.actions_d = load_coltans_traj(reference_traj_path)
+        self.initState = self.states_d[0]
+        self.state = self.initState
+        self.payloadStSize = 6
+        print(self.num_robots)
+        print(self.state)
+        self.states = np.zeros(
+            (len(self.states_d), self.payloadStSize + 6 * self.num_robots + 7 * self.num_robots)
+        )
+        self.states[0] = self.initState
+        self.appSt = []
+        self.appU = []
+        self.u = 0
+
+        self.dt = model["dt"]
+
     def step(self, action):
-        # Do something, depending on the action and current value of mu the next state is computed
-        return self._get_obs(), reward, done, truncated, info
+
+        payload_pos = self.state[0:3]
+
+        xnext = self.states[self.steps + 1]
+        x = self.states[self.steps]
+
+        self.robot.step(xnext, x, action, self.dt)
+        self.steps += 1
+
+        self.state = xnext
+        self.u = action
+        self.appSt.append(self.state.tolist())
+        self.appU.append(self.u.tolist())
+
+        observation = self._get_obs()
+        reward = self._get_reward(action, payload_pos)
+        done, distance_truncated, time_limited_truncated = self._get_done_or_truncated(payload_pos)
+        info = self._get_info(reward, done, distance_truncated, time_limited_truncated)
+
+        return observation, reward, done, distance_truncated or time_limited_truncated, info
 
     def _get_obs(self):
-
+        return self.state
 
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
-        # Choose the agent's location uniformly at random
-        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
-
-        # We will sample the target's location randomly until it does not coincide with the agent's location
-        self._target_location = self._agent_location
-        while np.array_equal(self._target_location, self._agent_location):
-            self._target_location = self.np_random.integers(
-                0, self.size, size=2, dtype=int
-            )
+        self.state = self.initState
+        self.payloadStSize = 6
+        self.states = np.zeros(
+            (len(self.states_d), self.payloadStSize + 6 * self.num_robots + 7 * self.num_robots)
+        )
+        self.states[0] = self.initState
+        self.appSt = []
+        self.appU = []
+        self.u = 0
+        self.steps = 0
 
         observation = self._get_obs()
         info = self._get_info()
 
         return observation, info
+
+    def _next_d_state_and_action(self):
+        return self.states_d[self.steps], self.actions_d[self.steps]
+
+    def _get_info(self, reward=0, done=False, distance_truncated=False, time_limited_truncated=False):
+        payload_pos_error = np.linalg.norm(self.states_d[self.steps, 0:3] - self.state[0:3])
+        info = {
+            "reward": reward,
+            "payload_pos_error": payload_pos_error
+        }
+        if done:
+            info["done"] = True
+            info["terminal_observation"] = self.state
+        if distance_truncated:
+            info["distance_truncated"] = True
+        if time_limited_truncated:
+            info["TimeLimit.truncated"] = True
+
+        return info
+
+    def _get_done_or_truncated(self, payload_pos):
+        done = False
+        distance_truncated = False
+        time_limited_truncated = False
+        final_payload_pos = self.states_d[-1, 0:3]
+        payload_pos = self.state[0:3]
+        payload_distance = np.linalg.norm(final_payload_pos - payload_pos)
+        payload_pos_error = np.linalg.norm(self.states_d[self.steps, 0:3] - self.state[0:3])
+        if self.steps >= len(self.actions_d):
+            if payload_distance < 0.01:
+                done = True
+            else:
+                time_limited_truncated = True
+
+        if payload_pos_error > 0.1:
+            done = True
+
+        return done, distance_truncated, time_limited_truncated
+
+    def _get_reward(self, action, payload_pos_before):
+        payload_pos_after = self.state[0:3]
+        final_payload_pos = self.states_d[-1, 0:3]
+
+        distance_before = np.linalg.norm(final_payload_pos - payload_pos_before)
+        distance_after = np.linalg.norm(final_payload_pos - payload_pos_after)
+
+        ctrl_cost = self._control_cost(action)
+
+        reward = - ctrl_cost
+
+        if distance_after < 0.01:
+            reward += 5.0
+
+        return reward
+
+    def _control_cost(self, action):
+        ctrl_cost_weight = 0.1
+        control_cost = ctrl_cost_weight * np.sum(np.square(action))
+        return control_cost
